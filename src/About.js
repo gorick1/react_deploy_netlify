3c4f8a5431551d4a1ccbc19734a92f8bb07133df
gorick1 <rhythmpaudel12@gmail.com>
2024-09-08 14:19:44 +0545

    second

--- src/About.js
+++ src/About.js
@@ -2,7 +2,7 @@ const About = () => {
   return (
       <main className='About'>
           <h2>About</h2>
-          <p style={{ marginTop: "1rem" }}>This simple react app is created by Rhythm Paudel. Follow me on <a href="https://www.linkedin.com/in/rhythmpaudel" target="_blank">LinkedIn </a> </p>
+          <p style={{ marginTop: "1rem" }}>This simple react app is created by Rhythm Paudel. Follow me on <a href="https://www.linkedin.com/in/rhythmpaudel" target="_blank" rel="noreferrer">LinkedIn </a> </p>
       </main>
   )
 }
