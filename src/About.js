04c5d7936ca3bf503f6417b26d8e7cc3cadd2e47
gorick1 <rhythmpaudel12@gmail.com>
2024-09-08 13:30:53 +0545

    git-commit

---
+++ src/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+      <main className='About'>
+          <h2>About</h2>
+          <p style={{ marginTop: "1rem" }}>This simple react app is created by Rhythm Paudel. Follow me on <a href="https://www.linkedin.com/in/rhythmpaudel" target="_blank">LinkedIn </a> </p>
+      </main>
+  )
+}
+
+export default About
